c62bd7d59ea1888a339f76821f790097044d180c
Maksim Budzko <MBudko@alfabank.ru>
2021-08-26 00:56:45 +0300

    TASK-2 add validation for password

--- swiftfoundation/swiftfoundation/FieldsValidator.swift
+++ swiftfoundation/swiftfoundation/FieldsValidator.swift
@@ -25,7 +25,7 @@ class FieldsValidator  {
     // Пароль (От 8 до 14 символов,  допустимые символы: цифры, буквы, специальные символы (выбери любые сам),
     // обязательные правила: минимум 1 буква большая, минимум 1 буква маленькая, минимум 1 цифра, минимум 1 специальный символ)+
     // positive look ahead
-    func isValidPassword(password: String) -> Bool {
+    func isValidPassword(password: String) -> Bool { 
         let regexPatternPassword =
             // from 8 to 14 characters
             #"(?=.{8,14})"# +
